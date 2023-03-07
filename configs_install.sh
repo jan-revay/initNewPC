1bd87767cfea1630e842a7a07a45ac9d8ca06125
jan.revay <jan.revay.tutor@gmail.com>
2023-03-07 23:23:46 +0100

    WIP version of the configs_install.sh script

---
+++ configs_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+mkdir ~/books
+mkdir ~/c        # code
+
+git config --global core.editor "vim"
+git config --global user.name "jan.revay"
+git config --global user.email "jan.revay.tutor@gmail.com"
