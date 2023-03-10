69d77a6e1b4e236083f37808f0a03e8c15404299
JohnnyR <jan.revay.tutor@gmail.com>
2023-03-10 11:32:31 +0100

    WIP autostart.sh list of apps and positioning

---
+++ config_files/autostart.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+nautilus &
+nautilus &
+google-chrome &
+firefox &
+code &
+code &
+signal-desktop &
+todoist &
+caprine &
+
+echo "ALL APPS STARTED"
+
+./position_windows.sh
+
+# spotify &
